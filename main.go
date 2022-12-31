c132a6824e65f900081bc2eaa3237570712b7faa
fastbpmn <fastbpmn@gmail.com>
2022-12-31 13:26:27 +0800

    feat: 新增NewGoodBye

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
 	http.ListenAndServe(":9090", sm)
 }
